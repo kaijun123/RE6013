49d621fcafb6e1908eb8e15e4f84017f372a4e18
kaijun123 <kaijun123@gmail.com>
2025-04-06 11:30:25 +0800

    add gemini and paynow code

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import request,render_template
+import os
+import google.generativeai as genai
 
+#makersuite_api = ""
+makersuite_api = os.getenv("makersuite")
+genai.configure(api_key=makersuite_api)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/",methods=["GET","POST"])
 def index():
-    return render_template('index.html')
+    return(render_template("index.html"))
+
+@app.route("/makersuite",methods=["GET","POST"])
+def makersuite():
+    return(render_template("makersuite.html"))
+
+@app.route("/gemini_reply",methods=["GET","POST"])
+def gemini_reply():
+    q = request.form.get("q")
+    print(q)
+    r = model.generate_content(q)
+    return(render_template("gemini_reply.html",r=r.candidates[0].content.parts[0].text))
+
+@app.route("/paynow",methods=["GET","POST"])
+def paynow():
+    return(render_template("paynow.html"))
 
 if __name__ == "__main__":
     app.run()
